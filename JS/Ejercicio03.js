bff2fad764cb6f8430d018529b8d6b982e429e9b
Diego Miguel Rivera Chávez <230260@utxicotepec.edu.mx>
2024-09-19 17:15:58 -0600

    Practica 5
    Agregación del Ejercicio05 tipo de dato Object

--- JS/Ejercicio03.js
+++ JS/Ejercicio03.js
@@ -1,5 +1,40 @@
 //Objetos
 console.warn("--- Tipo de dato: Object");
+const producto1 = {
+    Nombre:"Tablet\"9",
+    Marca:"Mac",
+    Modelo:"iPad",
+    CostoCompra: 11500.25,
+    CostoVenta: 15400,
+    Disponible: true,
+    SKU: Symbol("RICD040330HPLVHGA8"),
+    Colores: ["Blanco", "Negro", "Rosa", "Azul","Amarillo"]
+}
+//Imprimir el objeto
+console.log(producto1);
+//Los objetos se pueden imprimir en forma de tabla utilizando la funcion console.table
+console.table(producto1);
+
+//Acceder a las propiedades de un objeto
+console.warn("Leyendo las propiedades de un Objeto y sus tipos de datos")
+console.log(`Nombre del producto: ${producto1.Nombre}, que es del tipo: ${typeof(producto1.Nombre)}\n`+
+`Marca del producto: ${producto1.Marca}, que es del tipo: ${typeof(producto1.Marca)}`+
+`Marca del producto: ${producto1.Modelo}, que es del tipo: ${typeof(producto1.Modelo)}`+
+`Marca del producto: ${producto1.CostoCompra}, que es del tipo: ${typeof(producto1.CostoCompra)}`+
+`Marca del producto: ${producto1.CostoVenta}, que es del tipo: ${typeof(producto1.CostoVenta)}`+
+`Marca del producto: ${producto1.Disponible}, que es del tipo: ${typeof(producto1.Disponible)}`+
+`Marca del producto: ${String(producto1.SKU)}, que es del tipo: ${typeof(String(producto1.SKU))}`+
+`Marca del producto: ${producto1.Colores}, que es del tipo: ${typeof(producto1.Colores)}`);
+
+
+
+
+
+
+
+
+
+console.warn("--- Separacion ---");
 const producto = {
     nombre : "Tablet",
     precio : 300,
